78b7ae94c68e8efaf72bb996dc3a9159de7f5337
GabriellGimenes <gabrielgimenes157@gmail.com>
2024-02-19 15:09:06 -0300

    fix: useEffect in dropdown function

--- src/components/reports/tableReport/tableReport.tsx
+++ src/components/reports/tableReport/tableReport.tsx
@@ -10,7 +10,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { DailyItem } from "@/data/type";
+import { DailyData, DailyItem } from "@/data/type";
 
 import {
   DropdownMenu,
@@ -34,8 +34,14 @@ import {
 import React from "react";
 import { ThemeToggle } from "../../themeToggle";
 import { DatePicker } from "../../dataPickerRange";
-import { DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuLabel, DropdownMenuRadioGroup, DropdownMenuRadioItem, DropdownMenuSeparator } from "@/components/ui/dropdownMenu";
-
+import {
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdownMenu";
 
 interface DataItemProps {
   columns: ColumnDef<DailyItem, any>[];
@@ -44,51 +50,54 @@ interface DataItemProps {
 
 type Checked = DropdownMenuCheckboxItemProps["checked"];
 
+interface DataTypeProps {
+  columns: ColumnDef<DailyData, any>[];
+  data: DailyData[];
+}
 
-
-
-export function GenericDataItem({ columns, data }: DataItemProps) {
+export function GenericDataItem(
+  { columns: columnsDataItem, data: dataItem }: DataItemProps,
+  { columns: columnsDataType, data: dataType }: DataTypeProps
+) {
   const [sorting, setSorting] = React.useState<SortingState>([]);
-  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
-    []
-  );
-  const [showTypeDrying, setShowTypeDrying] = React.useState<Checked>(true)
-  const [showTypeProcessing, setShowTypeProcessing] = React.useState<Checked>(true)
-  const [showTypeMine, setShowTypeMine] = React.useState<Checked>(true)
+  const [columnFilters, setColumnFilters] = 
+    React.useState<ColumnFiltersState>([]);
+
+  const [showTypeDrying, setShowTypeDrying] = 
+    React.useState<Checked>(true);
+
+  const [showTypeProcessing, setShowTypeProcessing] =
+    React.useState<Checked>();
+
+  const [showTypeMine, setShowTypeMine] = 
+    React.useState<Checked>();
 
   const [columnVisibility, setColumnVisibility] =
     React.useState<VisibilityState>({});
 
-  const [tableData, setTableData] = React.useState<DailyItem[]>(data);
+  const [tableDataItem, setTableDataItem] =
+    React.useState<DailyItem[]>(dataItem);
 
-  // React.useEffect(() => {
-  //   const filterData = () => {
-  //     const filteredData = data.filter(item => {
-  //       if (showTypeDrying && item.name === "Drying") return true;
-  //       if (showTypeProcessing && item.name === "Processing") return true;
-  //       if (showTypeMine && item.name === "Mine") return true;
-  //       return false;
-  //     });
-  //     setTableData(filteredData);
-  //   };
-  // }, [data, showTypeDrying, showTypeProcessing, showTypeMine]);
+  const [tableDataType, setTableDataType] =
+    React.useState<DailyData[]>(dataType);
 
   React.useEffect(() => {
     // Filtrar os dados com base nas opções selecionadas
-    const filteredData = data.filter(item => {
-      if (showTypeDrying && item.name === "Drying") return true;
-      if (showTypeProcessing && item.name === "Processing") return true;
-      if (showTypeMine && item.name === "Mine") return true;
+    const filteredData = dataType.filter((item) => {
+      if (showTypeDrying && item.hasOwnProperty("drying")) return true;
+      if (showTypeProcessing && item.hasOwnProperty("processing")) return true;
+      if (showTypeMine && item.hasOwnProperty("mine")) return true;
       return false;
     });
+    console.log(filteredData);
 
     // Atualizar os dados da tabela
-    setTableData(filteredData);
-  }, [data, showTypeDrying, showTypeProcessing, showTypeMine]);
+    setTableDataType(filteredData);
+  }, [dataType, showTypeDrying, showTypeProcessing, showTypeMine]);
 
   const table = useReactTable({
-    data: tableData,
-    columns,
+    data: dataItem,
+    columns: columnsDataItem,
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
     getSortedRowModel: getSortedRowModel(),
@@ -108,60 +117,59 @@ export function GenericDataItem({ columns, data }: DataItemProps) {
   //   const filteredData = filterData(data); // Reaplica o filtro aos dados
   //   table.setTableData(filteredData); // Define os dados filtrados na tabela
   // };
-  
+
   return (
     <div>
       <div className="flex items-center py-4 gap-2">
         <Input
           placeholder="Filtrar por nome"
-          value={
-            (table.getColumn("name")?.getFilterValue() as string) || ""
-          }
+          value={(table.getColumn("name")?.getFilterValue() as string) || ""}
           onChange={(e) => {
             table.getColumn("name")?.setFilterValue(e.target.value);
-            console.log(e.target.value)
+            console.log(e.target.value);
           }}
           className="max-w-sm"
         />
-        <Button className="ml-4">
-          Export to PDF
-        </Button>
-        <ThemeToggle className="ml-4"/>
+        <Button className="ml-4">Export to PDF</Button>
+        <ThemeToggle className="ml-4" />
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <Button variant="outline" className="ml-4 w-32" >Report</Button>
+            <Button variant="outline" className="ml-4 w-32">
+              Report
+            </Button>
           </DropdownMenuTrigger>
           <DropdownMenuContent className="w-auto">
             <DropdownMenuLabel>Select the report</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuCheckboxItem
-                checked={showTypeDrying}
-                onCheckedChange={(checked) => {
-                  setShowTypeDrying(checked);
-                }}
-              >
-                Drying
-              </DropdownMenuCheckboxItem>
-              <DropdownMenuCheckboxItem
-                checked={showTypeProcessing}
-                onCheckedChange={(checked) => {
-                  setShowTypeProcessing(checked);
-                }}
-              >
-                Processing
-              </DropdownMenuCheckboxItem>
-              <DropdownMenuCheckboxItem
-                checked={showTypeMine}
-                onCheckedChange={(checked) => {setShowTypeMine(checked)
-                }}
-              >
-                Mine
-              </DropdownMenuCheckboxItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuCheckboxItem
+              checked={showTypeDrying}
+              onCheckedChange={(checked) => {
+                setShowTypeDrying(checked);
+              }}
+            >
+              Drying
+            </DropdownMenuCheckboxItem>
+            <DropdownMenuCheckboxItem
+              checked={showTypeProcessing}
+              onCheckedChange={(checked) => {
+                setShowTypeProcessing(checked);
+              }}
+            >
+              Processing
+            </DropdownMenuCheckboxItem>
+            <DropdownMenuCheckboxItem
+              checked={showTypeMine}
+              onCheckedChange={(checked) => {
+                setShowTypeMine(checked);
+              }}
+            >
+              Mine
+            </DropdownMenuCheckboxItem>
           </DropdownMenuContent>
         </DropdownMenu>
-        <DatePicker/>
+        <DatePicker />
       </div>
-      <div >
+      <div>
         <Table className="border">
           <TableHeader className="w-full">
             {table.getHeaderGroups().map((headerGroup) => {
@@ -169,10 +177,11 @@ export function GenericDataItem({ columns, data }: DataItemProps) {
                 <TableRow key={headerGroup.id}>
                   {headerGroup.headers.map((header) => {
                     const isParentHeader =
-                      header.column.columns &&
-                      header.column.columns.length > 0; 
+                      header.column.columns && header.column.columns.length > 0;
                     const colSpanValue = isParentHeader
-                      ? header.column.columns.length === 4 ? 10 : header.column.columns.length
+                      ? header.column.columns.length === 4
+                        ? 10
+                        : header.column.columns.length
                       : undefined;
                     return (
                       <TableHead
@@ -195,25 +204,28 @@ export function GenericDataItem({ columns, data }: DataItemProps) {
               );
             })}
           </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.map((row) => (
-            <TableRow key={row.id} className="hover:bg-cyan-50">
-              {row.getVisibleCells().map((cell) => {
-                const hasAct = cell.column.columnDef.header === "ACT";
-                const border = hasAct ? "border-l" : "";
-                          return (
-                  <TableCell key={cell.id} className={border}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                );
-              })}
-            </TableRow>
-          )) ?? (
-            <TableRow>
-              <TableCell>No results</TableCell>
-            </TableRow>
-          )}
-        </TableBody>
+          <TableBody>
+            {table.getRowModel().rows?.map((row) => (
+              <TableRow key={row.id} className="hover:bg-cyan-50">
+                {row.getVisibleCells().map((cell) => {
+                  const hasAct = cell.column.columnDef.header === "ACT";
+                  const border = hasAct ? "border-l" : "";
+                  return (
+                    <TableCell key={cell.id} className={border}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  );
+                })}
+              </TableRow>
+            )) ?? (
+              <TableRow>
+                <TableCell>No results</TableCell>
+              </TableRow>
+            )}
+          </TableBody>
         </Table>
       </div>
       <div className="flex items-center justify-start space-x-2 py-4">
